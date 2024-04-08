f049ee234341ace73671d92a459805c09a39901f
TALHA\talha <ttalhanazli@gmail.com>
2024-04-08 08:42:15 +0300

    +add static files, res index

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const photo = {
-        id:1,
-        name: 'Photo Name',
-        description: 'Photo description'
-    }
-    res.status(200).send(photo);
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;

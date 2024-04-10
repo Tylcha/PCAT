bbe92254364b176567feb2e266cdb75ad5ce560b
TALHA\talha <ttalhanazli@gmail.com>
2024-04-10 08:10:14 +0300

    +add mongo db, mongoose, create schema, routing, post method, show mainpage mongodb data

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import fs from 'fs';
 import path from 'path';
 import ejs from 'ejs';
 import { fileURLToPath } from 'url';
+import { Photo } from './models/Photo.js';
+import mongoose from 'mongoose';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 const app = express();
 
@@ -13,16 +16,32 @@ app.set('view engine', 'ejs');
 
 //MIDDLAWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.render('index');
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', { photos });
+    // console.log(photos);
 });
 app.get('/about', (req, res) => {
     res.render('about');
 });
 app.get('/add', (req, res) => {
     res.render('add');
+});
+// app.post('/photos', (req,res) =>{
+//     console.dir(req.ip);
+//     console.log(req.body);
+//     res.redirect('/')
+// })
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 const port = 3000;

c5a94a3396c50c3cf117497dcc176fbc4f5f7b48
TALHA\talha <ttalhanazli@gmail.com>
2024-05-04 03:07:42 +0300

    + bug fixes

--- models/connect.js
+++ models/connect.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const conn = () => {
-    mongoose.connect(process.env.DBI_URL);
+const conn = async () => {
+    try {
+        await mongoose.connect(process.env.DBI_URL, {
+            dbName: 'pcat',
+            serverApi: { version: '1', strict: true, deprecationErrors: true },
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default conn;
